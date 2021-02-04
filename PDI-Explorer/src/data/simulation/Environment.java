e7bd6d1b4bf4c5b844b55012e80ef2eeacd9af63
LCoquet <coquet.leo18@gmail.com>
2021-02-05 00:55:57 +0100

    Modified Environment.
    It is now a singleton. Every entities are stored in a single ArrayList.
    State has been removed and is only in simulation.

--- PDI-Explorer/src/data/simulation/Environment.java
+++ PDI-Explorer/src/data/simulation/Environment.java
@@ -5,52 +5,52 @@ import java.util.ArrayList;
 import data.entity.*;
 import data.map.Map;
 
-/**
- * 
+/** 
  * Contains all the data needed for a simulation.
  * 
- * Environment has four states :
- * 0. Finished
- * 1. Running
- * 2. Paused
- * 3. Stopped
+ * This class has to be instanced only once.
+ * Consequently, this is a Singleton class.
  * 
  * @author Léo COQUET
  *
  */
 
-public class Environment {
+public final class Environment {
 
+	//The only instance of Environment
+	private static volatile Environment instance = null;
+	
 	private Map map;
-	private ArrayList<Explorer> explorers;
+	private ArrayList<Entity> entities;
 	private int explorerAmount;
 	private int explorerInit;
-	private ArrayList<LivingEntity> animals;
-	private ArrayList<Chest> chests;
 	private int chestAmount;
 	private int foundChest;
 	private ArrayList<Item> items;
-	private ArrayList<Entity> obstacles;
-	private ArrayList<BreakableEntity> breakableObstacles;
-	private int state;
+	
+	private Environment() {}
 	
 	public Map getMap() {
 		return map;
 	}
-	public ArrayList<Explorer> getExplorers() {
-		return explorers;
+	
+	public final static Environment getInstance() {
+		if(Environment.instance == null )
+			synchronized(Environment.class) {
+				if(Environment.instance == null)
+					Environment.instance = new Environment();
+			}
+		return Environment.instance;
+	}
+	
+	public ArrayList<Entity> getEntities(){
+		return entities;
 	}
 	public int getExplorerAmount() {
 		return explorerAmount;
 	}
 	public int getExplorerInit() {
 		return explorerInit;
-	}
-	public ArrayList<LivingEntity> getAnimals() {
-		return animals;
-	}
-	public ArrayList<Chest> getChests() {
-		return chests;
 	}
 	public int getChestAmount() {
 		return chestAmount;
@@ -61,32 +61,17 @@ public class Environment {
 	public ArrayList<Item> getItems() {
 		return items;
 	}
-	public ArrayList<Entity> getObstacles() {
-		return obstacles;
-	}
-	public ArrayList<BreakableEntity> getBreakableObstacles() {
-		return breakableObstacles;
-	}
-	public int getState() {
-		return state;
-	}
 	public void setMap(Map map) {
 		this.map = map;
 	}
-	public void setExplorers(ArrayList<Explorer> explorers) {
-		this.explorers = explorers;
+	public void setEntities(ArrayList<Entity> entities) {
+		this.entities = entities;
 	}
 	public void setExplorerAmount(int explorerAmount) {
 		this.explorerAmount = explorerAmount;
 	}
 	public void setExplorerInit(int explorerInit) {
 		this.explorerInit = explorerInit;
-	}
-	public void setAnimals(ArrayList<LivingEntity> animals) {
-		this.animals = animals;
-	}
-	public void setChests(ArrayList<Chest> chests) {
-		this.chests = chests;
 	}
 	public void setChestAmount(int chestAmount) {
 		this.chestAmount = chestAmount;
@@ -96,15 +81,6 @@ public class Environment {
 	}
 	public void setItems(ArrayList<Item> items) {
 		this.items = items;
-	}
-	public void setObstacles(ArrayList<Entity> obstacles) {
-		this.obstacles = obstacles;
-	}
-	public void setBreakableObstacles(ArrayList<BreakableEntity> breakableObstacles) {
-		this.breakableObstacles = breakableObstacles;
-	}
-	public void setState(int state) {
-		this.state = state;
 	}
 	public void decrementExplorerAmount() {
 		if(explorerAmount > 0)
