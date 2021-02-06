b3526492b513cb5d4f07bfba08be21f5cbfcbfb1
LCoquet <coquet.leo18@gmail.com>
2021-02-06 15:31:08 +0100

    Modified Animal

--- PDI-Explorer/src/data/entity/Animal.java
+++ PDI-Explorer/src/data/entity/Animal.java
@@ -1,17 +1,9 @@
 package data.entity;
 
-import process.EntityVisitor;
-
 public abstract class Animal extends LivingEntity {
 
 	public Animal(String type, double[] position, double[] size, int maxHealth, int speed, int damage, int scope) {
 		super(type, position, size, maxHealth, speed, damage, scope);
-	}
-
-	@Override
-	public <T> T accept(EntityVisitor<T> visitor) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
