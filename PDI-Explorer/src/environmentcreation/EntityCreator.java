29d471f8c25cc26ee6e4665061189b70ac5a269a
LCoquet <coquet.leo18@gmail.com>
2021-02-06 15:44:35 +0100

    Added Obstacles Creation

--- PDI-Explorer/src/environmentcreation/EntityCreator.java
+++ PDI-Explorer/src/environmentcreation/EntityCreator.java
@@ -7,7 +7,7 @@ import environmentcreation.event.EntityCreationException;
 
 public class EntityCreator {
 
-	public static ArrayList<LivingEntity> creation(int explorerAmount, int animalAmount, int chestAmount) throws EntityCreationException{
+	public static ArrayList<LivingEntity> creation(int explorerAmount, int animalAmount) throws EntityCreationException{
 		
 		ArrayList<LivingEntity> al = new ArrayList<LivingEntity>();
 		al.addAll(ExplorersCreator.creation(explorerAmount));
