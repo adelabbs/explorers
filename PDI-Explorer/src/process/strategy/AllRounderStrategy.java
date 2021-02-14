6c4c4dadb98098e89f91732b350c939fa4f2c93b
GeoffroyK <geoffroy.keime@gmail.com>
2021-02-14 02:58:50 +0100

    scope circle fix

--- PDI-Explorer/src/process/strategy/AllRounderStrategy.java
+++ PDI-Explorer/src/process/strategy/AllRounderStrategy.java
@@ -2,6 +2,7 @@ package process.strategy;
 
 import java.util.ArrayList;
 
+import data.entity.Entity;
 import data.entity.LivingEntity;
 import data.simulation.Environment;
 import process.action.ExplorerMoveAction;
@@ -14,6 +15,7 @@ public class AllRounderStrategy extends ExplorationStrategy {
 	private double[] position;
 	private int scope;
 	private ArrayList<LivingEntity> inScopeEntities;
+	private ArrayList<Entity> inScopeObstacles;
 
 	public AllRounderStrategy(ExplorerManager explorerManager) {
 		super(explorerManager);
@@ -22,10 +24,10 @@ public class AllRounderStrategy extends ExplorationStrategy {
 	
 	@Override
 	public void decide() { 
-		inScopeEntities.removeAll(inScopeEntities);
+		//inScopeEntities.removeAll(inScopeEntities);
 		updateValues();
 		//If there is no living entity in scope then
-		if(inScopeEntities.isEmpty()) {
+		if(inScopeEntities.isEmpty() && inScopeObstacles.isEmpty()) {
 			//Set random movement action
 			MoveAction action = new ExplorerMoveAction(getExplorerManager().getExplorer(),
 					Environment.getInstance());
@@ -52,16 +54,17 @@ public class AllRounderStrategy extends ExplorationStrategy {
 		em = getExplorerManager();
 		position = em.getExplorer().getPosition();
 		scope = em.getExplorer().getScope();
-		inScopeEntities = getAllEntitiesInScope(scope);
+		inScopeEntities = getAllLivingEntitiesInScope(scope);
+		inScopeObstacles = getAllObstaclesInScope(scope);
 	}
 	
 	
 	/**
-	 * Get all living entities in the explorer's scope
+	 * Get all living entities in explorer's scope
 	 * @param scope
 	 * @return
 	 */
-	public ArrayList<LivingEntity> getAllEntitiesInScope(int scope){
+	public ArrayList<LivingEntity> getAllLivingEntitiesInScope(int scope){
 		int dx = (int) position[0] + scope;
 		int mdx = (int) position[0] - scope;
 		int dy = (int) position[1] + scope;
@@ -74,6 +77,31 @@ public class AllRounderStrategy extends ExplorationStrategy {
 				if(le.getPosition()[1] <= dy && le.getPosition()[1] >= mdy) {
 					if(!(position == le.getPosition())) {
 						inScope.add(le);
+					}
+				}
+			}
+		}
+		return inScope;
+	}
+	
+	/**
+	 * Get all non living entities in explorer's scope
+	 * @param scope
+	 * @return
+	 */
+	public ArrayList<Entity> getAllObstaclesInScope(int scope){
+		int dx = (int) position[0] + scope;
+		int mdx = (int) position[0] - scope;
+		int dy = (int) position[1] + scope;
+		int mdy = (int) position[1] - scope;
+		
+		ArrayList<Entity> entities = Environment.getInstance().getObstacles();
+		ArrayList<Entity> inScope = new ArrayList<Entity>();
+		for (Entity e : entities) {
+			if(e.getPosition()[0] <= dx && e.getPosition()[0] >= mdx) {
+				if(e.getPosition()[1] <= dy && e.getPosition()[1] >= mdy) {
+					if(!(position == e.getPosition())) {
+						inScope.add(e);
 					}
 				}
 			}
