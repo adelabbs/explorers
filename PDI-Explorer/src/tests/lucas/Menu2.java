0626470ffab8d1a74e0cc6167e307c703d2ddfc9
lespi <lespi@DESKTOP-4PPDA5P>
2021-02-14 14:54:21 +0100

    Menu with almost operational item selection

--- PDI-Explorer/src/tests/lucas/Menu2.java
+++ PDI-Explorer/src/tests/lucas/Menu2.java
@@ -1,5 +1,6 @@
 package tests.lucas;
 
+import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,9 +8,12 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
 
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 import data.simulation.Item;
 import data.simulation.SimulationEntry;
@@ -17,9 +21,22 @@ import process.itemCreation.ItemFactory;
 
 public class Menu2 extends JPanel implements ActionListener, ItemListener{
 	
-	private JCheckBox Binoculars = new JCheckBox("Binoculars");
-	private JCheckBox Knife = new JCheckBox("Knife");
-	private JCheckBox Antenna = new JCheckBox("Antenna");
+	JRadioButton Binoculars = new JRadioButton("Binoculars");
+	JRadioButton Glasses = new JRadioButton("Glasses");
+	JRadioButton Helmet = new JRadioButton("Helmet");
+	JRadioButton Chestplate = new JRadioButton("Chestplate");
+	JRadioButton Boots = new JRadioButton("Boots");
+	JRadioButton Trecking_Shoes = new JRadioButton("Trecking Shoes");
+	JRadioButton Knife = new JRadioButton("Knife");
+	JRadioButton Machete = new JRadioButton("Machete");
+	JRadioButton Antenna = new JRadioButton("Antenna");
+	JRadioButton Radio = new JRadioButton("Radio");
+	JPanel health = new JPanel();
+	JPanel speed = new JPanel();
+	JPanel damage = new JPanel();
+	JPanel scope = new JPanel();
+	JPanel com = new JPanel();
+	
 	
 	private String[] nbExplorers = { "3", "4", "5", "6" };
 	private SimulationEntry sim = new SimulationEntry(0,0,10,0);
@@ -28,28 +45,76 @@ public class Menu2 extends JPanel implements ActionListener, ItemListener{
 	public Menu2 () {
 		this.setLayout(new FlowLayout());
 		
+		health.setLayout(new BoxLayout(health, BoxLayout.Y_AXIS));
+		speed.setLayout(new BoxLayout(speed, BoxLayout.Y_AXIS));
+		damage.setLayout(new BoxLayout(damage, BoxLayout.Y_AXIS));
+		scope.setLayout(new BoxLayout(scope, BoxLayout.Y_AXIS));
+		com.setLayout(new BoxLayout(com, BoxLayout.Y_AXIS));
+		
 		JComboBox explorerChoice = new JComboBox(nbExplorers);
 		explorerChoice.setSelectedIndex(0);
 		explorerChoice.addActionListener(this);
 		
 		this.add(explorerChoice);
 
-		Binoculars.setMnemonic(KeyEvent.VK_C); 
 		Binoculars.setSelected(false);
-
-	    Knife.setMnemonic(KeyEvent.VK_G); 
 	    Knife.setSelected(false);
-
-	    Antenna.setMnemonic(KeyEvent.VK_H); 
 	    Antenna.setSelected(false);
+	    Glasses.setSelected(false);
+	    Machete.setSelected(false);
+	    Radio.setSelected(false);
+	    Helmet.setSelected(false);
+	    Trecking_Shoes.setSelected(false);
+	    Chestplate.setSelected(false);
+	    Boots.setSelected(false);
 
 	    Binoculars.addItemListener(this);
 	    Knife.addItemListener(this);
-	    Antenna.addItemListener(this); 
+	    Antenna.addItemListener(this);
+	    Glasses.addItemListener(this);
+	    Machete.addItemListener(this);
+	    Radio.addItemListener(this);
+	    Helmet.addItemListener(this);
+	    Trecking_Shoes.addItemListener(this);
+	    Chestplate.addItemListener(this);
+	    Boots.addItemListener(this);
 	    
-	    this.add(Binoculars);
-	    this.add(Knife);
-	    this.add(Antenna);
+	    health.add(Helmet);
+	    health.add(Chestplate);
+	    speed.add(Boots);
+	    speed.add(Trecking_Shoes);
+	    damage.add(Knife);
+	    damage.add(Machete);
+	    scope.add(Glasses);
+	    scope.add(Binoculars);
+	    com.add(Antenna);
+	    com.add(Radio);
+	    
+	    this.add(health);
+	    this.add(speed);
+	    this.add(damage);
+	    this.add(scope);
+	    this.add(com);
+	    
+	    ButtonGroup healthGroup = new ButtonGroup();
+	    healthGroup.add(Helmet);
+	    healthGroup.add(Chestplate);
+
+	    ButtonGroup speedGroup = new ButtonGroup();
+	    speedGroup.add(Boots);
+	    speedGroup.add(Trecking_Shoes);
+	    
+	    ButtonGroup damageGroup = new ButtonGroup();
+	    damageGroup.add(Knife);
+	    damageGroup.add(Machete);
+	    
+	    ButtonGroup scopeGroup = new ButtonGroup();
+	    scopeGroup.add(Glasses);
+	    scopeGroup.add(Binoculars);
+	    
+	    ButtonGroup comGroup = new ButtonGroup();
+	    comGroup.add(Antenna);
+	    comGroup.add(Radio);
 	    
 	    animalAmount = (int) (2 + (Math.random() * (6 - 2)));
 	    
@@ -62,24 +127,26 @@ public class Menu2 extends JPanel implements ActionListener, ItemListener{
 
 	    	if (e.getStateChange() == ItemEvent.SELECTED) {
 	    		if (source == Binoculars) {
-	    			//Item binoculars = ItemFactory.create("Binoculars");
 	    			sim.add("Binoculars");
 	    		} else if (source == Knife) {
-	    			//Item knife = ItemFactory.create("Knife");
 	    			sim.add("Knife");
 	    		} else if (source == Antenna) {
-	    			//Item antenna = ItemFactory.create("Antenna");
 	    			sim.add("Antenna");
+	    		} else if (source == Glasses) {
+	    			sim.add("Glasses");
+	    		} else if (source == Helmet) {
+	    			sim.add("Helmet");
+	    		} else if (source == Chestplate) {
+	    			sim.add("Chestplate");
+	    		} else if (source == Boots) {
+	    			sim.add("Boots");
+	    		} else if (source == Trecking_Shoes) {
+	    			sim.add("Trecking_Shoes");
+	    		} else if (source == Machete) {
+	    			sim.add("Machete");
+	    		} else if (source == Radio) {
+	    			sim.add("Radio");
 	    		}
-	    	}
-	    	else if (e.getStateChange() == ItemEvent.DESELECTED) {
-	    		if (source == Binoculars) {
-	    			sim.getItems().remove("Binoculars");
-		    	} else if (source == Knife) {
-		    		sim.getItems().remove("Knife");
-		    	} else if (source == Antenna) {
-		    		sim.getItems().remove("Antenna");
-		    	}
 	    	}
 	    }
 
