848616571482ac0888a73ec705108a93a943a6f4
LCoquet <coquet.leo18@gmail.com>
2021-02-14 13:07:24 +0100

    Added new Items

--- PDI-Explorer/src/process/itemCreation/ItemFactory.java
+++ PDI-Explorer/src/process/itemCreation/ItemFactory.java
@@ -6,27 +6,80 @@ public class ItemFactory {
 
 	public static Item create(String name) {
 		switch(name) {
+		//Health items
+		case "Helmet" :
+			return helmetCreation();
+		case "Chestplate" :
+			return chestplateCreation();
+			
+		//Speed items
+		case "Boots" :
+			return bootsCreation();
+		case "Trecking shoes" :
+			return treckingShoesCreation();
+		
+		//Damage items
 		case "Knife" :
 			return knifeCreation();
+		case "Machete" :
+			return macheteCreation();
+			
+		//Vision items
+		case "Glasses" :
+			return glassesCreation();
 		case "Binoculars" :
 			return binocularsCreation();
+			
+		//Communication range items
 		case "Antenna" :
 			return antennaCreation();
+		case "Radio" :
+			return radioCreation();
+			
+		//make an exception
 		default :
 			return null;
 		}
 	}
 	
+	//Health items
+	private static Item helmetCreation() {
+		return new Item("Helmet", "Health", 5);
+	}
+	private static Item chestplateCreation() {
+		return  new Item("Chestplate", "Health", 10);
+	}
+	
+	//Speed items
+	private static Item bootsCreation() {
+		return new Item("Boots", "Speed", 1);
+	}
+	private static Item treckingShoesCreation() {
+		return new Item("Trecking Shoes", "Speed", 2);
+	}
+	
+	//Damage items
 	private static Item knifeCreation() {
 		return new Item("Knife", "Damage", 1);
 	}
-	
-	private static Item binocularsCreation() {
-		return new Item("Binoculars", "Scope", 1);
+	private static Item macheteCreation() {
+		return new Item("Machete", "Damage", 3);
 	}
 	
+	//Vision items
+	private static Item glassesCreation() {
+		return new Item("Glasses", "Scope", 1);
+	}
+	private static Item binocularsCreation() {
+		return new Item("Binoculars", "Scope", 2);
+	}
+	
+	//Communication range items
 	private static Item antennaCreation() {
 		return new Item("Antenna", "Communication Range", 1);
+	}
+	private static Item radioCreation() {
+		return new Item("Radio", "Communication Range", 2);
 	}
 	
 }
