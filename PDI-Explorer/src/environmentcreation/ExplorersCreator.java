cb5eece5e1663d588ef6f39791c9344aa5d01803
LCoquet <coquet.leo18@gmail.com>
2021-02-14 02:15:28 +0100

    Explorer's name implementation

--- PDI-Explorer/src/environmentcreation/ExplorersCreator.java
+++ PDI-Explorer/src/environmentcreation/ExplorersCreator.java
@@ -1,5 +1,9 @@
 package environmentcreation;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import data.entity.Explorer;
@@ -15,11 +19,25 @@ public class ExplorersCreator {
 
 	public static ArrayList<Explorer> creation(int explorerAmount){
 
+		String line = null;
+		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("ressources/names.csv"));
+			line = reader.readLine();
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		String[] names = line.split(",");
+		
 		ExplorerPlacer ep = new ExplorerPlacer(explorerAmount);
 		
 		ArrayList<Explorer> explorers = new ArrayList<>();
 		for(int i = 0; i < explorerAmount; i ++)
-		explorers.add(new Explorer(ep.place(), "Roger"));
+			explorers.add(new Explorer(ep.place(), names[i]));
 		return explorers;
 		
 	}
