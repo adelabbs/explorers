688cde557953ef2c7f56f405bd84f7218f66f2b5
LCoquet <coquet.leo18@gmail.com>
2021-03-06 18:09:51 +0100

    Fixed nounours
    The nounours now follow the explorer, and can detect imprecisely an
    explorer on his territory.

--- PDI-Explorer/src/process/action/AnimalMoveAction.java
+++ PDI-Explorer/src/process/action/AnimalMoveAction.java
@@ -1,7 +1,9 @@
 package process.action;
 
 import data.entity.Animal;
+import data.entity.Entity;
 import data.simulation.Environment;
+import process.SimulationUtility;
 
 public class AnimalMoveAction extends MoveAction {
 
@@ -23,28 +25,50 @@ public class AnimalMoveAction extends MoveAction {
 	@Override
 	public void execute() {
 		double newPos[];
-		do {
+		double closestHumanPos[];
+		if((closestHumanPos = closestHumanInScope()) != null) {
 			newPos = new double[] {getEntity().getPosition()[0], getEntity().getPosition()[1]};
-			int direction = (int) (Math.random() * 4);
-			if (direction == 4)
-				direction = 3;
-			switch (direction) {
-			case 0:
-				newPos[0]++;
-				break;
-			case 1:
-				newPos[0]--;
-				break;
-			case 2:
-				newPos[1]++;
-				break;
-			case 3:
-				newPos[1]--;
-				break;
+			double vx = closestHumanPos[0] - entity.getPosition()[0];
+			double vy = closestHumanPos[1] - entity.getPosition()[1];
+			if(Math.abs(vx) > Math.abs(vy)) {
+				if(vx < 0) {
+					newPos[0] --;
+				} else {
+					newPos[0] ++;
+				}
+			} else {
+				if(vy < 0) {
+					newPos[1] --;
+				} else {
+					newPos[1] ++;
+				}
 			}
-		} while (distanceFromInitialPos(newPos) > ANIMAL_MAX_TERRITORY && !outOfBorder(newPos));
+		}
+		else {
+			do {
+				newPos = new double[] {getEntity().getPosition()[0], getEntity().getPosition()[1]};
+				int direction = (int) (Math.random() * 4);
+				if (direction == 4)
+					direction = 3;
+				switch (direction) {
+				case 0:
+					newPos[0]++;
+					break;
+				case 1:
+					newPos[0]--;
+					break;
+				case 2:
+					newPos[1]++;
+					break;
+				case 3:
+					newPos[1]--;
+					break;
+				}
+			} while (distanceFromInitialPos(newPos) > ANIMAL_MAX_TERRITORY && !outOfBorder(newPos));
+		}
 		getEntity().setPosition(newPos);
 	}
+		
 
 	private double distanceFromInitialPos(double newPos[]) {
 		return Math.sqrt(Math.pow(newPos[0] - getEntity().getInitPosition()[0], 2)
@@ -60,4 +84,40 @@ public class AnimalMoveAction extends MoveAction {
 	public Animal getEntity() {
 		return entity;
 	}
+	
+	private double[] closestHumanInScope() {
+		double[] closestPosition = null;
+		double maxDistance = 1000;
+		for(Entity e : Environment.getInstance().getEntities()) {
+			if(e.getType().equals("Explorer")) {
+				double distance = SimulationUtility.distance(entity.getPosition(), e.getPosition());
+				if(distance < maxDistance
+						&& distanceFromInitialPos(e.getPosition()) < ANIMAL_MAX_TERRITORY) {
+					if(distance < entity.getScope()) {
+						maxDistance = distance;
+						closestPosition = new double[] {e.getPosition()[0], e.getPosition()[1]};
+					} else {
+						maxDistance = imprecise(distance);
+						closestPosition = imprecise(e.getPosition());
+					}					
+				}
+			}
+		}
+		return closestPosition;
+	}
+	
+	private double imprecise(double value) {
+		double gap = Math.random() * 2* value/10;
+		gap -= value/10;
+		return value + gap;
+	}
+	
+	private double[] imprecise(double[] values) {
+		double[] copiedValues = new double[] {values[0], values[1]};
+		for(int i = 0; i < values.length; i ++) {
+			copiedValues[i] = imprecise(copiedValues[i]);
+		}
+		return copiedValues;
+	}
+
 }
