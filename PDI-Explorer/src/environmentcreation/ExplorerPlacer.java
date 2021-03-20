372b0dbc79cafe5b6f6748de6c7a67ae007dedff
LCoquet <coquet.leo18@gmail.com>
2021-03-20 11:09:56 +0100

    Bug correction
    No crash at the placement of the explorers

--- PDI-Explorer/src/environmentcreation/ExplorerPlacer.java
+++ PDI-Explorer/src/environmentcreation/ExplorerPlacer.java
@@ -23,20 +23,44 @@ public class ExplorerPlacer {
 	
 	private void configuration(int explorerAmount) {
 		Map map = Environment.getInstance().getMap();
+		
+		//places explorers on top of the map
 		int sent = 0;
 		while(map.getTile(sent, sent).getType().equals("w"))
 			sent ++;
 		double[] last = new double[] {(double)(sent), (double)(sent)};
 		positions.add(new double[] {last[0], last[1]});
-		for(int i = 0; i < explorerAmount - 1; i ++) {
-			last[1] += 8.0;
-			while(map.getTile((int) last[0], (int) last[1]).getType().equals("w"))
-				last[1] ++;
-			positions.add(new double[] {last[0], last[1]});
-//			System.out.prSintln(last[0] + " " + last[1]);
+		int i = 0;
+		while(i < explorerAmount - 1 && !oob(last)) {
+			last[1] += 15.0;
+			boolean placeable = false;
+			while(!oob(last) && !placeable)
+				if(map.getTile((int) last[0], (int) last[1]).getType().equals("w"))
+					last[1] ++;
+				else placeable = true;
+			if(placeable) {
+				positions.add(new double[] {last[0], last[1]});
+				i ++;
+			}
 		}
-//		for(double[] d : positions)
-//			System.out.println(d[0] + " " + d[1]);
+		
+		//places explorers on bottom of the map
+		sent = 89;
+		while(map.getTile(sent, sent).getType().equals("w"))
+			sent --;
+		last = new double[] {(double)(sent), (double)(sent)};
+		positions.add(new double[] {last[0], last[1]});
+		while(i < explorerAmount - 1 && !oob(last)) {
+			last[1] -= 15.0;
+			while(map.getTile((int) last[0], (int) last[1]).getType().equals("w"))
+				last[1] --;
+			positions.add(new double[] {last[0], last[1]});
+			i ++;
+		}
+	}
+	
+	private boolean oob(double[] pos) {
+		return pos[0] < 0 || pos[0] >= 90 || pos[1] < 0 || pos[1] >= 90;
 	}
 	
 }
