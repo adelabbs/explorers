9929d5f68756657dbda68953830a71f693e63289
lespi <lespi@DESKTOP-4PPDA5P>
2021-04-06 13:06:29 +0200

    Deleted useless package

--- PDI-Explorer/src/tests/geoffroy/TestSimulation.java
+++
@@ -1,9 +0,0 @@
-package tests.geoffroy;
-
-public class TestSimulation {
-	public static void main(String[] args) {
-		TestGUI simulationGUI = new TestGUI();
-		Thread thread = new Thread(simulationGUI);
-		thread.start();
-	}
-}
